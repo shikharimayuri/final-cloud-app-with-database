b5ad333598f2c770a26178d9348cf2b3072b0a96
Mayuri Shikhari <shikharimayuri@gmail.com>
2026-09-10 19:43:48 +0530

    Fix exam result scoring

--- onlinecourse/views.py
+++ onlinecourse/views.py
@@ -38,22 +38,22 @@ def show_exam_result(request, course_id, submission_id):
     choices = submission.choices.all()
 
     total_score = 0
-    questions = course.question_set.all()  # Assuming course has related questions
+    possible = 0
+    selected_ids = [choice.id for choice in choices]
+
+    questions = course.question_set.all()
 
     for question in questions:
-        correct_choices = question.choice_set.filter(is_correct=True)  # Get all correct choices for the question
-        selected_choices = choices.filter(question=question)  # Get the user's selected choices for the question
-
-        # Check if the selected choices are the same as the correct choices
-        if set(correct_choices) == set(selected_choices):
-            total_score += question.grade  # Add the question's grade only if all correct answers are selected
+        possible += question.grade
+        if question.is_get_score(choices):
+            total_score += question.grade
 
     context['course'] = course
+    context['selected_ids'] = selected_ids
     context['grade'] = total_score
-    context['choices'] = choices
+    context['possible'] = possible
 
     return render(request, 'onlinecourse/exam_result_bootstrap.html', context)
-
 def registration_request(request):
     context = {}
     if request.method == 'GET':
